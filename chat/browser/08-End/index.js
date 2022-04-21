8eceba6c5cd50facbd88e09ded889d114cae249e
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-04-21 17:32:33 +0200

    fixes to work

--- chat/browser/08-End/index.js
+++ chat/browser/08-End/index.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime.js'
-import React from 'react'
+import React from "react";
 import ReactDOM from 'react-dom'
 import App from './common/views/App'
 import createLibp2p from './libp2p'
